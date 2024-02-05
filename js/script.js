ffa3f4d905870fb67db03069ee9b7fcfa73acd23
Rafael Reyes <100984393+raffeee@users.noreply.github.com>
2024-02-05 14:12:05 +0800

    initial setup w/ notes

---
+++ js/script.js
@@ -0,0 +1,18 @@
+let canvasSize = 16;
+
+function createCanvas(size) {
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            // create a tile div
+        }
+        // create a new row of tile divs
+    }
+}
+
+/*
+NOTES
+
+nested for loop that creates a square div
+what should be the dimensions? relative?
+
+*/
