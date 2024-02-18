9d55b3280b7e4c1ca575c9e17dabbf37259a4799
Rafael Reyes <100984393+raffeee@users.noreply.github.com>
2024-02-18 13:00:55 +0800

    feat: coloring tiles on mouse click

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-const canvasSize = 16;
 const canvas = document.querySelector("#canvas")
+let canvasSize = 16;
+let isDrawing = false;
 
 function createCanvas(size) {
     for (let i = 0; i < size*size; i++) {
@@ -7,11 +8,22 @@ function createCanvas(size) {
         tile.classList.add('tile');
         tile.style.width = `calc(100% / ${size}`;
         tile.style.height = `calc(100% / ${size}`;
+        tile.addEventListener('mouseover', () => {
+            if (isDrawing) {
+                tile.style.backgroundColor = `#000000`;
+            }
+        })
+        tile.addEventListener('mousedown', () => {
+            isDrawing = true;
+        })
+        tile.addEventListener('mouseup', () => {
+            isDrawing = false;
+        })
         canvas.appendChild(tile);
     }
 }
 
-// test if works
+// step 4 func
 function updateCanvasSize(size) {
     canvasSize = size;
 }
